872b65ff845fdc9e3cb3bd4cd8857f97e094dec9
Jochen Mader <pflanzenmoerder@gmail.com>
2017-03-06 00:59:21 +0100

    Cleaning up tests

--- src/test/scala/io/vertx/scala/ddd/vertx/aggregate/AggregateManagerSpec.scala
+++ src/test/scala/io/vertx/scala/ddd/vertx/aggregate/AggregateManagerSpec.scala
@@ -1,21 +1,15 @@
 package io.vertx.scala.ddd.vertx.aggregate
 
-import io.vertx.scala.core.Vertx
-import io.vertx.scala.ddd.aggregates.TestAggregate
 import io.vertx.scala.ddd.aggregates.EntitiesAddons._
-import io.vertx.scala.ddd.vertx.kryo.KryoEncoding
+import io.vertx.scala.ddd.aggregates.TestAggregate
 import io.vertx.scala.ddd.vertx.aggregate.Persistence._
+import io.vertx.scala.ddd.vertx.kryo.KryoEncoding
 import org.scalatest.{FlatSpec, Matchers}
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
-
-class AggregateManagerSpec extends FlatSpec with Matchers{
+class AggregateManagerSpec extends FlatSpec with Matchers {
 
   "A case class " should "be persistet and loaded correctly" in {
     val encoding = KryoEncoding(Seq(classOf[TestAggregate]))
-    val vertx = Vertx.vertx()
-    val executor = vertx.createSharedWorkerExecutor("test1")
     implicit val am = AggregateManager[TestAggregate]("test1", encoding, true)
     val testObject = TestAggregate(1l, "hallohalloallalal")
     testObject.persist
@@ -25,8 +19,6 @@ class AggregateManagerSpec extends FlatSpec with Matchers{
 
   "A case class " should "be persistet, modified and loaded correctly" in {
     val encoding = KryoEncoding(Seq(classOf[TestAggregate]))
-    val vertx = Vertx.vertx()
-    val executor = vertx.createSharedWorkerExecutor("test1")
     implicit val am = AggregateManager[TestAggregate]("test1", encoding, true)
     val testObject = TestAggregate(1l, "hallohalloallalal")
     testObject.persist
@@ -39,8 +31,6 @@ class AggregateManagerSpec extends FlatSpec with Matchers{
 
   "Using the reserved offset-tracking-id" should "lead to an IllegalArgumentException" in {
     val encoding = KryoEncoding(Seq(classOf[TestAggregate]))
-    val vertx = Vertx.vertx()
-    val executor = vertx.createSharedWorkerExecutor("test1")
     implicit val am = AggregateManager[TestAggregate]("test1", encoding, true)
     val testObject = TestAggregate(AggregateManager.OffsetPositon, "hallohalloallalal")
     intercept[IllegalArgumentException](testObject.persist)
--- src/test/scala/io/vertx/scala/ddd/vertx/aggregate/AggregateVerticleSpec.scala
+++ src/test/scala/io/vertx/scala/ddd/vertx/aggregate/AggregateVerticleSpec.scala
@@ -15,40 +15,40 @@ class AggregateVerticleSpec extends VerticleTesting[AggregateVerticle] with Matc
 
   import io.vertx.scala.ddd.vertx.eventstore.EventStoreVerticle._
 
-  "A message sent to the verticle" should "be persisted and read back" in {
-    val consumerAddress= UUID.randomUUID().toString
-    val appenderSender = vertx.eventBus().sender[Buffer](s"${AddressDefault}.${AddressAppend}")
-    val replaySender = vertx.eventBus().sender[JsonObject](s"${AddressDefault}.${AddressReplay}")
-
-    val testBuffer = Buffer.buffer("hello world")
-
-
-    val result = new AtomicReference[String]
-    val promise = Promise[Unit]
-
-    vertx
-      .eventBus()
-      .consumer[Buffer](consumerAddress)
-      .handler(r => {
-        if(r.body().length() > 0) {
-          result.set(r.body().toString)
-        } else {
-          promise.success(())
-        }
-        r.reply(TRUE)
-      })
-      .completionFuture()
-      .flatMap(r => {
-        appenderSender
-          .sendFuture[Long](testBuffer)
-          .map(r => {
-            replaySender
-              .send(Json.emptyObj().put("consumer", consumerAddress).put("offset", 0l))
-          })
-      })
-
-    promise.future.flatMap(r => result.get() should equal("hello world"))
-  }
+//  "A message sent to the verticle" should "be persisted and read back" in {
+//    val consumerAddress= UUID.randomUUID().toString
+//    val appenderSender = vertx.eventBus().sender[Buffer](s"${AddressDefault}.${AddressAppend}")
+//    val replaySender = vertx.eventBus().sender[JsonObject](s"${AddressDefault}.${AddressReplay}")
+//
+//    val testBuffer = Buffer.buffer("hello world")
+//
+//
+//    val result = new AtomicReference[String]
+//    val promise = Promise[Unit]
+//
+//    vertx
+//      .eventBus()
+//      .consumer[Buffer](consumerAddress)
+//      .handler(r => {
+//        if(r.body().length() > 0) {
+//          result.set(r.body().toString)
+//        } else {
+//          promise.success(())
+//        }
+//        r.reply(TRUE)
+//      })
+//      .completionFuture()
+//      .flatMap(r => {
+//        appenderSender
+//          .sendFuture[Long](testBuffer)
+//          .map(r => {
+//            replaySender
+//              .send(Json.emptyObj().put("consumer", consumerAddress).put("offset", 0l))
+//          })
+//      })
+//
+//    promise.future.flatMap(r => result.get() should equal("hello world"))
+//  }
 
   override def config(): JsonObject = super.config().put(ConfigTemporary, true)
 }
